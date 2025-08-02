04fc6028b087e687e811ac06701b06bc5fa78f30
djs-zmtc [RAZORBACK] <github@zmtcreative.com>
2025-08-02 14:27:34 -0400

    mod: added nsis and upx options to build and compute sha1 and sha256 on installer

--- Build-WailsApp.ps1
+++ Build-WailsApp.ps1
@@ -13,6 +13,15 @@ if ( $(git describe --tags HEAD 2> $null) -match "v?\d+\.\d+\.\d+(-\w+)?") {
 # echo $Date
 # echo $Commit
 
-Push-Location $PSScriptRoot
-wails build -ldflags "-X main.Version=${Version} -X main.Date=${Date} -X main.Commit=${Commit}"
-Pop-Location
+Push-Location $PSScriptRoot -StackName "project-root"
+wails build -clean -ldflags "-X main.Version=${Version} -X main.Date=${Date} -X main.Commit=${Commit}" -nsis -upx
+Set-Location ./build/bin
+foreach ($file in Get-ChildItem *-installer.exe -File -ErrorAction SilentlyContinue) {
+    $sha256Name = $file.Name + ".sha256"
+    $sha1Name = $file.Name + ".sha1"
+    $sha256Hash = (Get-FileHash $file -Algorithm SHA256).Hash # | ForEach-Object { $_.Hash } | Out-File -FilePath $sha256Name -Encoding utf8
+    $sha1Hash = (Get-FileHash $file -Algorithm SHA1).Hash # | ForEach-Object { $_.Hash } | Out-File -FilePath $sha1Name -Encoding utf8
+    "$sha256Hash  *$($file.Name)" | Out-File -FilePath $sha256Name -Encoding utf8
+    "$sha1Hash  *$($file.Name)" | Out-File -FilePath $sha1Name -Encoding utf8
+}
+Pop-Location -StackName "project-root"
