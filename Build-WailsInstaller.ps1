58cdc6e6c59b695bec6ed74d54a9a4bf05165cdc
djs-zmtc [RAZORBACK] <github@zmtcreative.com>
2025-08-03 15:13:44 -0400

    mod: add repo checks and other enhancements

--- Build-WailsInstaller.ps1
+++ Build-WailsInstaller.ps1
@@ -6,27 +6,32 @@
         This script builds the Wails application, updates the version in
         wails.json based on the current Git commit, and generates SHA256
         and SHA1 hashes for the installer files.
+
+        By default, it shows the current version and file version without building.
 #>
 
 #Requires -Version 7.0
 
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $false, HelpMessage = "Skip the build process and just update the wails.json file.")]
-    [Alias("n")]
-    [switch]$NoBuild,
-    [Parameter(Mandatory = $false, HelpMessage = "Skip the NSIS installer generation and just build the application.")]
-    [Alias("e","exe","app")]
-    [switch]$ExeOnly,
-    [Parameter(Mandatory = $false, HelpMessage = "(Implies -NoBuild) Skip the version update in wails.json and just show the current version. Useful for checking the current version without making changes.")]
-    [Alias("s","v","show")]
-    [switch]$ShowVersionOnly,
-    [Parameter(Mandatory = $false, HelpMessage = "Use UPX to compress the executable files.")]
-    [Alias("u")]
+    [Parameter(Mandatory = $false, HelpMessage = "Run the build process without generating the NSIS installer.")]
+    [Alias("b","exe")]
+    [switch]$Build,
+    [Parameter(Mandatory = $false, HelpMessage = "(Implies -Build) Create the NSIS installer after building.")]
+    [Alias("i","installer")]
+    [switch]$NSIS,
+    [Parameter(Mandatory = $false, HelpMessage = "(Implies -Build) Use UPX to compress the executable file.")]
+    [Alias("u","c","compress","compact")]
     [switch]$UPX
 )
 
+$ShowVersionOnly = $true
+if ($Build -or $NSIS -or $UPX) {
+    $ShowVersionOnly = $false
+}
 Set-Location $PSScriptRoot
+
+$ScriptName = $MyInvocation.MyCommand.Name
 
 function Get-JsonContent {
     [CmdletBinding()]
@@ -83,6 +88,58 @@ function Get-DateStamp {
     return int($result)
 }
 
+function Confirm-RepositoryIsClean {
+    $status = git status --porcelain
+    if ($status) {
+        $isClean = $true
+        $statusList = $status -split "`n"
+        $newStatusList = @()
+        foreach ($line in $statusList) {
+            if ( -not ($line -match $ScriptName) ) {
+                $isClean = $false
+                $newStatusList += $line
+            }
+        }
+
+        if ($isClean) { return $true }
+
+        Write-Host ""
+        Write-Host -ForegroundColor Red "WARNING: Repository is not clean. Please commit or stash your changes before building."
+        Write-Host ""
+        Write-Host "Uncommitted changes:"
+        Write-Host ""
+        $newStatusList | ForEach-Object { Write-Host -ForegroundColor Yellow "  $_" }
+        Write-Host ""
+        Write-Host "Suggestions:"
+        Write-Host "  - Commit your changes: git commit -m 'Your commit message'"
+        Write-Host "  - Create a new branch: git checkout -b new-branch-name "
+        Write-Host "       and commit your changes to the branch"
+        Write-Host "  - Stash your changes: git stash --all"
+        Write-Host "  - Discard your changes: git reset --hard HEAD"
+        Write-Host ""
+        Write-Host -ForegroundColor Cyan "NOTE: Script ignores changes to the script itself ($ScriptName)"
+        Write-Host ""
+        return $false
+    }
+    return $true
+}
+
+function Restore-RepositoryToCleanState {
+    Push-Location $PSScriptRoot -StackName "restoreproject"
+    Write-Host -ForegroundColor Yellow "Restoring repository to a clean state..."
+    $FileList = @(
+        "wails.json",
+        "build/windows/installer/project.nsi"
+    )
+    foreach ($file in $FileList) {
+        if ( (git status --porcelain | Select-String "$file") ) {
+            Write-Host -ForegroundColor Yellow "  Restoring: $file"
+            git restore "$file"
+        }
+    }
+    Pop-Location -StackName "restoreproject"
+}
+
 function Update-ProjectNSI {
     param (
         [Parameter(Mandatory = $true)]
@@ -91,7 +148,7 @@ function Update-ProjectNSI {
     )
 
     if (-not (Test-Path -Path $ProjectNSIPath)) {
-        Write-Error "Project NSI file not found at path: $ProjectNSIPath"
+        Write-Host -ForegroundColor Red "Project NSI file not found at path: $ProjectNSIPath"
         return
     }
 
@@ -99,32 +156,85 @@ function Update-ProjectNSI {
 
     $NSIData = Get-Content -Path $ProjectNSIPath
     $NewNSIData = @()
+    $FileChanged = $false
 
     foreach ($line in $NSIData) {
-        if ($line -match '^(VIFileVersion\s+)"([^"]+)"\s*$') {
-            Write-Host -ForegroundColor Yellow "  Updating VIFileVersion in NSIS project file"
-            $newval1 = $Matches[1] + '"' + $FileVersion + '"'
-            $line = $line -replace '^(VIFileVersion\s+)"([^"]+)"\s*$', $newval1
+        if ($line -match '^(?<key>VIFileVersion\s+)"(?<value>[^"]+)"\s*$') {
+            if (string($Matches.key) -ne $FileVersion) {
+                $newval1 = $Matches.key + '"' + $FileVersion + '"'
+                $line = $line -replace '^(VIFileVersion\s+)"([^"]+)"\s*$', $newval1
+                Write-Host -ForegroundColor Yellow "  Updating VIFileVersion in NSIS project file"
+                $FileChanged = $true
+            }
         }
-        if ($line -match '^(VIProductVersion\s+)"([^"]+)"\s*$') {
-            Write-Host -ForegroundColor Yellow "  Updating VIProductVersion in NSIS project file"
-            $newval2 = $Matches[1] + '"' + $FileVersion + '"'
-            $line = $line -replace '^(VIProductVersion\s+)"([^"]+)"\s*$', $newval2
+        if ($line -match '^(?<key>VIProductVersion\s+)"(?<value>[^"]+)"\s*$') {
+            if (string($Matches.key) -ne $FileVersion) {
+                Write-Host -ForegroundColor Yellow "  Updating VIProductVersion in NSIS project file"
+                $newval2 = $Matches.key + '"' + $FileVersion + '"'
+                $line = $line -replace '^(VIProductVersion\s+)"([^"]+)"\s*$', $newval2
+                $FileChanged = $true
+            }
         }
-        if ($line -match '^(VIAddVersionKey\s+"FileVersion"\s+)"([^"]+)"\s*$') {
-            Write-Host -ForegroundColor Yellow "  Updating VIAddVersionKey FileVersion in NSIS project file"
-            $newval3 = $Matches[1] + '"' + $FileVersion + '"'
-            $line = $line -replace '^(VIAddVersionKey\s+"FileVersion"\s+)"([^"]+)"\s*$', $newval3
+        if ($line -match '^(?<key>VIAddVersionKey\s+"FileVersion"\s+)"(?<value>[^"]+)"\s*$') {
+            if (string($Matches.key) -ne $FileVersion) {
+                Write-Host -ForegroundColor Yellow "  Updating VIAddVersionKey FileVersion in NSIS project file"
+                $newval3 = $Matches.key + '"' + $FileVersion + '"'
+                $line = $line -replace '^(VIAddVersionKey\s+"FileVersion"\s+)"([^"]+)"\s*$', $newval3
+                $FileChanged = $true
+            }
         }
         $NewNSIData += $line
+    }
+    if (-not $FileChanged) {
+        Write-Host -ForegroundColor Green "  No changes made to NSIS project file"
+        return
     }
     Set-Content -Path $ProjectNSIPath -Encoding utf8 -Value $NewNSIData
 }
 
+function Update-WailsJSON {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$WailsJsonPath,
+        [string]$Version
+    )
+
+    if (-not (Test-Path -Path $WailsJsonPath)) {
+        Write-Host -ForegroundColor Red "wails.json file not found at path: $WailsJsonPath"
+        return
+    }
+
+    Write-Host -ForegroundColor Green "Updating wails.json with version value: $Version"
+    $WailsData = Get-JsonContent -Path $WailsJsonPath
+
+    if (-not $WailsData) {
+        Write-Host -ForegroundColor Red "  Failed to read wails.json or it is empty."
+        return
+    }
+    if ($WailsData.Info.productVersion -ne $Version) {
+        $WailsData.Info.productVersion = $Version
+        Set-JsonContent -Path $WailsJsonPath -Value $WailsData
+        Write-Host -ForegroundColor Green "  Version changed to: $Version"
+    } else {
+        Write-Host -ForegroundColor Yellow "  No changes made to wails.json, version is already set to: $Version"
+    }
+}
+
 function Invoke-WailsBuild {
-    $NSIS = "-nsis"
-    $UPX = ""
+    $NSISOption = ""
+    $UPXOption  = ""
     $ProjectNSI = Join-Path $PSScriptRoot "build" "windows" "installer" "project.nsi"
+    $WailsJsonPath = Join-Path $PSScriptRoot "wails.json"
+
+    if ($NSIS) {
+        $NSISOption = "-nsis"
+        $Build = $true
+    }
+    if ($UPX) {
+        $UPXOption = "-upx"
+        $Build = $true
+    }
+
     $Date = $(Get-Date -AsUTC -Format "yyyy-MM-ddTHH:mm:ssZ")
     $Version = ""
     $FileVersion = ""
@@ -164,28 +274,25 @@ function Invoke-WailsBuild {
 
 
     if ($ShowVersionOnly) {
-        $NoBuild = $true
+        $Build = $false
         Write-Host "   Current App version: $Version"
         Write-Host "     NSIS File version: $FileVersion"
         return
-    } else {
-        if ($ExeOnly) { $NSIS = "" } else {
-            Update-ProjectNSI -ProjectNSIPath $ProjectNSI -FileVersion $FileVersion
-        }
-
-        if ($UPX) { $UPX = "-upx" }
-
-        Write-Host -ForegroundColor Green "Updating wails.json with version value: $FileVersion"
-        $WailsJsonPath = Join-Path $PSScriptRoot "wails.json"
-        $WailsData = Get-JsonContent -Path $WailsJsonPath
-        $WailsData.Info.productVersion = $Version
-        Set-JsonContent -Path $WailsJsonPath -Value $WailsData
     }
 
-    if (! $NoBuild) {
+    if (-not (Confirm-RepositoryIsClean)) {
+        return
+    }
+
+    if ($Build) {
+        if ($NSIS) {
+            Update-ProjectNSI -ProjectNSIPath $ProjectNSI -FileVersion $FileVersion
+        }
+        Update-WailsJSON -WailsJsonPath $WailsJsonPath -Version $Version
+
         Push-Location $PSScriptRoot -StackName "project-root"
         Write-Host -ForegroundColor Green "Building Wails application with version value: $Version"
-        wails build -clean -ldflags "-X main.Version=${Version} -X main.Date=${Date} -X main.Commit=${Commit}" ${NSIS} ${UPX}
+        wails build -clean -ldflags "-X main.Version=${Version} -X main.Date=${Date} -X main.Commit=${Commit}" ${NSISOption} ${UPXOption}
         Set-Location ./build/bin
         foreach ($file in Get-ChildItem *-installer.exe -File -ErrorAction SilentlyContinue) {
             $sha256Name = $file.Name + ".sha256"
@@ -195,6 +302,7 @@ function Invoke-WailsBuild {
             "$sha256Hash  *$($file.Name)" | Out-File -FilePath $sha256Name -Encoding utf8
             "$sha1Hash  *$($file.Name)" | Out-File -FilePath $sha1Name -Encoding utf8
         }
+        Restore-RepositoryToCleanState
         Pop-Location -StackName "project-root"
     }
 }
