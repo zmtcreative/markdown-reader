e73410eaf717d320b33114d014adae9d15f7d29f
djs-zmtc [RAZORBACK] <github@zmtcreative.com>
2025-08-09 11:40:38 -0400

    add: update function for build/windows/info.json

--- scripts/New-GitTag.ps1
+++ scripts/New-GitTag.ps1
@@ -92,6 +92,7 @@ if (Test-Path -Path "$tmpProjectRoot\go.mod") {
 $FileList = @(
     "wails.json",
     "frontend/package.json",
+    "build/windows/info.json",
     "build/windows/installer/project.nsi"
 )
 
@@ -357,6 +358,57 @@ function Update-PackageJSON {
         Write-Host -ForegroundColor Green "  Version changed to: $Version"
     } else {
         Write-Host -ForegroundColor Yellow "  No changes made to package.json, version is already set to: $Version"
+    }
+}
+
+function Update-InfoJSON {
+    <#
+    .SYNOPSIS
+        Updates the info.json file with the specified version.
+    .DESCRIPTION
+        This function modifies the info.json file to set the correct version.
+    .PARAMETER InfoJsonPath
+        The path to the info.json file to update.
+    .PARAMETER Version
+        The new version to set in the info.json file.
+    #>
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$InfoJsonPath,
+        [Parameter(Mandatory = $true)]
+        [string]$TagName
+    )
+
+    if (-not (Test-Path -Path $InfoJsonPath)) {
+        Write-Host -ForegroundColor Red "info.json file not found at path: $InfoJsonPath"
+        return
+    }
+
+    $RC = @("alpha", "beta", "rc", "patch", "")
+    $tmpVersionHash = Get-VersionHash -TagName $TagName
+    $FileVersion = "$($tmpVersionHash.Major).$($tmpVersionHash.Minor).$($tmpVersionHash.Patch)"
+    if (-not [string]::IsNullOrWhiteSpace($tmpVersionHash.Prerelease)) {
+        if ($tmpVersionHash.Prerelease -match "^(?<rc>alpha|beta|rc)?(?<rcnum>\d+)?$") {
+            $rcString = $Matches.rc
+            $rcNumber = [int]$Matches.rcnum || 0
+            $rcIDX = $RC.IndexOf($rcString) + 1
+        }
+
+        $PreReleaseNumber = ($rcIDX * 10000) + ($rcNumber * 100)
+        # $tmpVersionHash.Prerelease = $PreReleaseNumber.ToString("D5")
+        $FileVersion += ".${PreReleaseNumber}"
+    }
+
+    Write-Host -ForegroundColor Cyan "Updating info.json:"
+    $VIData = Get-JsonContent -Path $InfoJsonPath
+
+    if (-not $VIData) {
+        Write-Host -ForegroundColor Red "  Failed to read info.json or it is empty."
+        return
+    }
+    $VIData.fixed.file_version = $FileVersion
+    if (Set-JsonContent -Path $InfoJsonPath -Value $VIData) {
+        Write-Host -ForegroundColor Green "  Updated info.json"
     }
 }
 
@@ -827,6 +879,7 @@ function Invoke-NewGitTag {
     $ProjectNSI = Join-Path $ProjectRoot "build" "windows" "installer" "project.nsi"
     $WailsJsonPath = Join-Path $ProjectRoot "wails.json"
     $PackageJsonPath = Join-Path $ProjectRoot "frontend" "package.json"
+    $InfoJsonPath = Join-Path $ProjectRoot "build" "windows" "info.json"
 
     if (-not (Confirm-RepositoryIsClean -IgnoreFileList)) {
         return
@@ -887,6 +940,7 @@ function Invoke-NewGitTag {
         Update-ProjectNSI -ProjectNSIPath $ProjectNSI -TagName $newTagName
         Update-WailsJSON -WailsJsonPath $WailsJsonPath -TagName $newTagName
         Update-PackageJSON -PackageJsonPath $PackageJsonPath -TagName $newTagName
+        Update-InfoJSON -InfoJsonPath $InfoJsonPath -TagName $newTagName
 
         if (-not (Push-RepositoryCommit -TagName $newTagName -Message $Message)) {
             Write-Host -ForegroundColor Red "Failed to commit changes before tagging. Exiting."
