ff6983776127fd720d379cc904feb653157012d5
djs-zmtc [RAZORBACK] <github@zmtcreative.com>
2025-08-10 07:54:10 -0400

    mod: change scss partials to use underscore filename syntax

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,5 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-// import './assets/css/colors-root-variables.css'
-// import './assets/css/colors-bg.css'
-// import './assets/css/colors-fg-text.css'
-// import './assets/css/styles.css';
-// import './assets/css/techdoc.css';
-// import './assets/css/print.css';
 import './assets/scss/index.scss';
 
 createApp(App).mount('#app')
